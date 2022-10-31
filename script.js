ad0e35e4ede77bcd9f151e2c6d0d17d7c6e7d4cd
Mohammed Al-Kawaz <84942933+muhammed-py@users.noreply.github.com>
2022-10-31 09:52:10 +0300

    fixing highscire
    adding a feature to save a high score

--- script.js
+++ script.js
@@ -1,160 +1,151 @@
-//valueAsNumber is the key  
- 
- 
-// ------------------------------------------------
+let x = Math.floor(Math.random() * 20 + 1),
+  body = document.querySelector("body"),
+  checkBtn = document.querySelector(".check"),
+  againBtn = document.querySelector(".again"),
+  helpBtn = document.querySelector(".help"),
+  square = document.getElementById("square"),
+  message = document.querySelector(".message"),
+  score = Number(document.getElementById("score").textContent),
+  scoreDisplay = document.getElementById("score"),
+  highScore = document.getElementById("highscore"),
+  title = document.getElementById("title"),
+  container = document.querySelector(".instruction_container"),
+  helpItems0 = document.querySelector(".instruction_element0"),
+  helpItems1 = document.querySelector(".instruction_element1"),
+  helpItems2 = document.querySelector(".instruction_element2"),
+  helpItems3 = document.querySelector(".instruction_element3"),
+  helpItems4 = document.querySelector(".instruction_element4"),
+  instructionBtn = document.querySelector(".instruction_button"),
+  arr = [];
 
- let x = Math.floor((Math.random() * 20) + 1);
 
- console.log(x);
+//----------------------------------------------
 
- let title = document.getElementById("title");
+console.log(x);
 
- let message = document.getElementById("message");
+//-----------------------------------------------
 
- let score = document.getElementById("score");
-
- let highScore = document.getElementById("high-score");
-
- let btnAgain = document.getElementById("button_again");
-
- let checkBtn = document.getElementById("button");
-
- let helpBtn = document.querySelector(".help");
-
- let arr = [];
-
- let max = 0;
-
- console.log(localStorage.getItem('save'));
-
- 
-
- 
-
- 
-
- 
-
- 
+function gameLost() {
+  title.textContent = " Loser !!";
+  square.textContent = x;
+  message.textContent = " You Have Lost !!";
+  body.style.backgroundColor = "red";
+  checkBtn.style.display = "none";
+}
 
 
 
+function guess() {
 
+  input = document.getElementById("user_input").valueAsNumber;
 
+  if (input < x) {
 
+    score = score - 1;
+    scoreDisplay.textContent = score;
 
+    if (input - x >= -2) {
 
- function help () {
-
-  
-   
-   // this function will show the help window
- }
-
- helpBtn.addEventListener('click', help)
-
- 
-
- 
- document.getElementById("button").onclick = function () {
-
-    let number = document.getElementById("user_input").valueAsNumber;
-
-
-    if ((Number(score.textContent) <=0)) {
-
-      document.body.style.backgroundColor="red";
-    
-      title.textContent="You Have LOST !!";
-    
-      square.textContent=x;
-    
-      message.textContent = "LOSER !";
-          
+      message.textContent = " Try a littel Higher"
     }
 
+    else {
 
-    else if (number === x) {
+      message.textContent = " Your number is too Low "
+    }
 
-       document.body.style.backgroundColor="green";
+    if (score == 0) {
+      gameLost();
+    }
+    return score
 
-       title.textContent="You Have Won !!";
+  }
 
-       square.textContent=x;
+  else if (input > x) {
 
-       message.textContent ="Your guess is CORRECT";
+    score = score - 1;
+    scoreDisplay.textContent = score;
 
-        checkBtn.style.display="none";
-                                
-    }   
-    
-    else if (number < x ) {
-            score.textContent= Number(score.textContent)-1;
-               
-              if ((x-number <= 2)) {message.textContent="Try a littel Higher";}
+    if (input - x <= 2) {
 
-              else {message.textContent="Your guess is too Low !!"}
+      message.textContent = " Try a littel lower"
+    }
 
-              
+    else {
+      message.textContent = " Your number is too high "
+    }
 
- }
+    if (score == 0) {
+      gameLost();
+    }
 
-          else {
-            score.textContent= Number(score.textContent)-1;
+    return score
 
-            if ((x-number >= -2)) {message.textContent="Try a littel Lower";}
+  }
 
-            else {message.textContent="Your guess is too High !!"}
+  else if (input == x) {
 
-            
+    title.textContent = " You have won !";
+    square.textContent = x;
+    message.textContent = " You are correct !";
+    body.style.backgroundColor = "Green";
+    checkBtn.style.display = "none";
 
-            
+    let scoreM = localStorage.getItem('score')  //scoreM = score memory
 
-          }
+    if (scoreM < score) {
+      localStorage.setItem('score', score)
+    }
+  }
+
+  else {
+
+    message.textContent = " Please enter a number ";
+  }
+}
+
+function reload() {
+
+  location.reload();
+  localStorage.removeItem('score');
+}
+
+function help() {
+
+  container.classList.remove("hidden");
+  helpItems0.classList.remove("hidden");
+  helpItems1.classList.remove("hidden");
+  helpItems2.classList.remove("hidden");
+  helpItems3.classList.remove("hidden");
+  helpItems4.classList.remove("hidden");
+  instructionBtn.classList.remove("hidden");
+
+}
+
+function unHide() {
+
+  container.classList.add("hidden");
+  helpItems0.classList.add("hidden");
+  helpItems1.classList.add("hidden");
+  helpItems2.classList.add("hidden");
+  helpItems3.classList.add("hidden");
+  helpItems4.classList.add("hidden");
+  instructionBtn.classList.add("hidden");
+
 }
 
 
 
 
-// Again button
 
-document.getElementById("button_again").onclick = function() {
+//-------------------------------------------------
 
-  
+highScore.textContent = localStorage.getItem('score');
 
-location.reload();
- 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(localStorage.getItem("save"));
-
-// highScore.textContent ="🥇 Highscore:" + localStorage.getItem("save");
-
-  
-
-
-
-
-
-
-
-
-
- 
-
-
+checkBtn.onclick = guess;
+againBtn.onclick = reload;
+helpBtn.onclick = help;
+instructionBtn.onclick = unHide;
 
 
 
